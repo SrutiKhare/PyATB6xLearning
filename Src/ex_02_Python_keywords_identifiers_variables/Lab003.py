8069a147db067fe1fad9d491f983350a40910279
sruti <srutikhare09@gmail.com>
2026-09-16 23:36:00 +0530

    New Files added

---
+++ Src/ex_02_Python_keywords_identifiers_variables/Lab003.py
@@ -0,0 +1,8 @@
+print(2+2)
+"""def print(*values: object,
+          sep: str | None = " ",
+          end: str | None = "\n",
+          file: SupportsWrite[str] | None = None,
+          flush: Literal[False] = False) -> None """
+print("Hello",123,"John")
+print("Hello",123,"John",sep="-")
