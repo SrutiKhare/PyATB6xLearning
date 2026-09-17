27ac516444e024f495adaa4c662755009d9341e7
sruti <srutikhare09@gmail.com>
2026-09-17 22:33:49 +0530

    New Files added

---
+++ Src/ex_02_Python_keywords_identifiers_variables/Lab004_keywords.py
@@ -0,0 +1,17 @@
+"""
+Python data type :
+    Numeric data type
+        int
+        float
+        string
+        Boolean
+        List
+        Dictionary etc
+Identifiers rules :
+'_' can be a variable name
+'-' can be a variable name
+'*' can be a variable name
+number cannot be first thing in the variable name or identifier
+Alphanumeric is possible as variable name
+
+"""
---
+++ Src/ex_02_Python_keywords_identifiers_variables/Lab005_Variables_part1.py
@@ -0,0 +1,14 @@
+age = 65
+_age = "boss"
+# Dynamic typed Language : Run time data type can be changed
+name="sruti"
+name="sruti1"
+name="sruti2"
+name="khare"
+_=12
+print(_)
+_=_+1
+print(name)
+print(age)
+print(_age)
+print(_)
